dd1c35ededf3e5beba54d40783abd4ed810669e2
Tim Trefren <ttrefren@gmail.com>
2010-09-18 18:10:17 -0700

    basic server done.. perhaps

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http')
-http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hi\n');
-}).listen(8124, "0.0.0.0");
-console.log('Server running');
